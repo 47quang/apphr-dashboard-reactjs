3a43f4c6e99e6d222f82cd37c9b01d3e5ece4514
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-03-04 15:24:36 +0700

    Commit: - Create 2 button in setting shift screen

--- src/components/toolbar/GridCreateToolbarButton.js
+++ src/components/toolbar/GridCreateToolbarButton.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {
+  AddCircleOutlineOutlined,
+  DeleteOutlineOutlined,
+} from "@material-ui/icons";
+import { COLORS } from "../../constants/theme";
 
-GridCreateToolbarButton.propTypes = {
-
-};
-
+GridCreateToolbarButton.propTypes = {};
+//TODO: translate
 function GridCreateToolbarButton(props) {
-    return (
-        <Link to="/setting/shift/newShift">new</Link>
-    );
+  return (
+    <div>
+      <Link to="/setting/shift/newShift">
+        <button
+          type="button"
+          className="btn"
+          style={{
+            color: COLORS.BUTTON_COLOR,
+            fontSize: 13,
+            alignSelf: "center",
+          }}
+        >
+          <AddCircleOutlineOutlined style={{ fontSize: 18 }} className="mr-1" />
+          NEW
+        </button>
+      </Link>
+      <button
+        type="button"
+        className="btn"
+        style={{
+          color: COLORS.BUTTON_COLOR,
+          fontSize: 13,
+          justifyContent:'center'
+        }}
+      >
+        <DeleteOutlineOutlined style={{ fontSize: 18 }} className="mr-1" />
+        DELETE
+      </button>
+    </div>
+  );
 }
 
 export default GridCreateToolbarButton;
