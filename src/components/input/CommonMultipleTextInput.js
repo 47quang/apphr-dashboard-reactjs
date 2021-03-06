e9c282622c051112150d8f4260ed6675d9d68bdb
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-03-06 17:58:34 +0700

    change profile setting

---
+++ src/components/input/CommonMultipleTextInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Label from "../label/label";
+
+const CommonMultipleTextInput = ({
+  containerClassName,
+  labelText,
+  inputClassName = "",
+  onChange,
+  onBlur,
+  value,
+  inputID,
+  rows = 5,
+}) => {
+  return (
+    <div className={containerClassName}>
+      <Label text={labelText} labelID={inputID} />
+      <textarea
+        className={inputClassName}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        id={inputID}
+        autoComplete={"off"}
+        rows={rows}
+      />
+    </div>
+  );
+};
+
+export default CommonMultipleTextInput;
