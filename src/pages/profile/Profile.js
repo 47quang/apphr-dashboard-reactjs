893c17710c2d3a50655b167640977b1ed9619c6f
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-03-06 16:36:36 +0700

    commit: - create feature/profile

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { CContainer } from "@coreui/react";
+import {
+  DataGrid,
+  GridColumnsToolbarButton,
+  GridToolbarContainer,
+} from "@material-ui/data-grid";
+import React, { useState } from "react";
+import { TheHeader } from "src/layouts";
 
+//TODO: translate
 const Profile = ({ t, location }) => {
-  return <div>This is Profile page</div>;
+  const [selectionModel, setSelectionModel] = useState([]);
+  const CustomToolbar = () => (
+    <GridToolbarContainer>
+      <GridColumnsToolbarButton displayName="Col"/>
+    </GridToolbarContainer>
+  );
+  const rows = [];
+  const columns = [
+    { field: "code", headerName: "Mã nhân viên", width: 150 },
+    { field: "employee_name", headerName: "Họ và tên", width: 200 },
+    { field: "phone", headerName: "Số điện thoại", width: 150 },
+    { field: "sex", headerName: "Giới tính", width: 110 },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "position", headerName: "Vị trí", width: 200 },
+    { field: "department", headerName: "Phòng ban", width: 200 },
+    { field: "branch", headerName: "Chi nhánh", width: 200 },
+  ];
+  return (
+    <>
+      <TheHeader />
+      <CContainer fluid className="c-main mb-3">
+        <DataGrid
+          className="bg-white py-3 px-4"
+          autoHeight
+          checkboxSelection
+          selectionModel={selectionModel}
+          pageSize={5}
+          rowsPerPageOptions={[5, 10]}
+          pagination
+          rows={rows}
+          columns={columns}
+          components={{
+            Toolbar: CustomToolbar,
+            // NoRowsOverlay: CustomNoRowsOverlay,
+          }}
+        />
+      </CContainer>
+    </>
+  );
 };
 
 export default Profile;
