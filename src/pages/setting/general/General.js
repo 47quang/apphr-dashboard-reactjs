5801d75fe25d121a8176f41e1fc1cc1af5fa2b6a
Quang Tran <quang.tran290699@gmail.com>
2021-03-14 09:41:16 +0700

    --no commit message

--- src/pages/setting/general/General.js
+++ src/pages/setting/general/General.js
@@ -1,49 +1,51 @@
-import { CContainer } from "@coreui/react";
-import { Formik } from "formik";
-import React, { useEffect, useRef, useState } from "react";
-import { useDispatch } from "react-redux";
-import CommonMultipleTextInput from "src/components/input/CommonMultipleTextInput";
-import CommonTextInput from "src/components/input/CommonTextInput";
-import CommonSelectInput from "src/components/input/CommonSelectInput";
-import BasicLoader from "src/components/loader/BasicLoader";
-import { SettingGeneralInfoSchema } from "src/schema/formSchema";
-import { changeListButtonHeader } from "src/stores/actions/header";
+import { CContainer } from '@coreui/react';
+import { Formik } from 'formik';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import CommonMultipleTextInput from 'src/components/input/CommonMultipleTextInput';
+import CommonTextInput from 'src/components/input/CommonTextInput';
+import CommonSelectInput from 'src/components/input/CommonSelectInput';
+import BasicLoader from 'src/components/loader/BasicLoader';
+import { SettingGeneralInfoSchema } from 'src/schema/formSchema';
+import { changeListButtonHeader } from 'src/stores/actions/header';
+import { fetchProvinces } from 'src/stores/actions/location';
 
 //TODO: translate
 const SettingGeneralPage = ({ t, location }) => {
   const companyInfoForm = useRef();
+  const dispatch = useDispatch();
   const [initialValues, setInitialValues] = useState({
-    companyName: "",
+    companyName: '',
     provinceId: 0,
     districtId: 0,
     wardId: 0,
-    address: "",
-    phone: "",
-    email: "",
-    shortName: "",
-    taxCode: "",
-    note: "",
+    address: '',
+    phone: '',
+    email: '',
+    shortName: '',
+    taxCode: '',
+    note: '',
   });
   const [isLoader, setIsLoader] = useState(true);
-  const dispatch = useDispatch();
   const getCompanyInfo = async () => {
     setInitialValues({
-      companyName: "APPHPR",
+      companyName: 'APPHPR',
       provinceId: 0,
       districtId: 0,
       wardId: 0,
-      address: "",
-      phone: "",
-      email: "",
-      shortName: "",
-      taxCode: "",
-      note: "",
+      address: '',
+      phone: '',
+      email: '',
+      shortName: '',
+      taxCode: '',
+      note: '',
     });
     setTimeout(() => {
       setIsLoader(false);
     }, 500);
   };
   useEffect(() => {
+    dispatch(fetchProvinces());
     getCompanyInfo();
     dispatch(
       changeListButtonHeader([
@@ -70,37 +72,37 @@ const SettingGeneralPage = ({ t, location }) => {
 
   const getListOfProvinces = () => {
     let lstOfProvinces = [
-      { id: 1, name: "Hồ Chí Minh" },
-      { id: 5, name: "Bình Dương" },
+      { id: 1, name: 'Hồ Chí Minh' },
+      { id: 5, name: 'Bình Dương' },
     ];
     return lstOfProvinces;
   };
   const getListOfDistricts = (provinceID) => {
-    if (provinceID === "1")
+    if (provinceID === '1')
       return [
-        { id: 1, name: "Quận 1" },
-        { id: 2, name: "Quận 2" },
-        { id: 3, name: "Quận 3" },
-        { id: 4, name: "Quận 4" },
-        { id: 5, name: "Quận 5" },
-        { id: 6, name: "Quận 10" },
-        { id: 10, name: "Quận Thủ Đức" },
+        { id: 1, name: 'Quận 1' },
+        { id: 2, name: 'Quận 2' },
+        { id: 3, name: 'Quận 3' },
+        { id: 4, name: 'Quận 4' },
+        { id: 5, name: 'Quận 5' },
+        { id: 6, name: 'Quận 10' },
+        { id: 10, name: 'Quận Thủ Đức' },
       ];
-    else if (provinceID === "5")
+    else if (provinceID === '5')
       return [
-        { id: 1, name: "Dĩ An" },
-        { id: 2, name: "Thủ Dầu Một" },
+        { id: 1, name: 'Dĩ An' },
+        { id: 2, name: 'Thủ Dầu Một' },
       ];
   };
 
   const getListOfWars = (districtID) => {
     let lstWars = [
-      { id: 1, name: "Phường 01" },
-      { id: 2, name: "Phường 02" },
-      { id: 3, name: "Phường 03" },
-      { id: 5, name: "Phường 05" },
-      { id: 7, name: "Phường 07" },
-      { id: 9, name: "Phường 09" },
+      { id: 1, name: 'Phường 01' },
+      { id: 2, name: 'Phường 02' },
+      { id: 3, name: 'Phường 03' },
+      { id: 5, name: 'Phường 05' },
+      { id: 7, name: 'Phường 07' },
+      { id: 9, name: 'Phường 09' },
     ];
     return lstWars;
   };
@@ -123,58 +125,58 @@ const SettingGeneralPage = ({ t, location }) => {
                 <form>
                   <div className="row">
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.companyName}
-                      onBlur={handleBlur("companyName")}
-                      onChange={handleChange("companyName")}
-                      inputID={"name"}
-                      labelText={"Tên doanh nghiệp"}
-                      inputType={"text"}
-                      placeholder={"Nhập tên doanh nghiệp"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('companyName')}
+                      onChange={handleChange('companyName')}
+                      inputID={'name'}
+                      labelText={'Tên doanh nghiệp'}
+                      inputType={'text'}
+                      placeholder={'Nhập tên doanh nghiệp'}
+                      inputClassName={'form-control'}
                       isRequiredField
                       isTouched={touched.companyName}
                       isError={errors.companyName && touched.companyName}
                       errorMessage={errors.companyName}
                     />
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.shortName}
-                      onBlur={handleBlur("shortName")}
-                      onChange={handleChange("shortName")}
-                      inputID={"shortName"}
-                      labelText={"Tên viết tắt của doanh nghiệp"}
-                      inputType={"text"}
-                      placeholder={"Nhập tên viết tắt"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('shortName')}
+                      onChange={handleChange('shortName')}
+                      inputID={'shortName'}
+                      labelText={'Tên viết tắt của doanh nghiệp'}
+                      inputType={'text'}
+                      placeholder={'Nhập tên viết tắt'}
+                      inputClassName={'form-control'}
                     />
                   </div>
                   <div className="row">
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.phone}
-                      onBlur={handleBlur("phone")}
-                      onChange={handleChange("phone")}
-                      inputID={"phone"}
-                      labelText={"Số điện thoại"}
-                      inputType={"text"}
-                      placeholder={"Nhập số điện thoại"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('phone')}
+                      onChange={handleChange('phone')}
+                      inputID={'phone'}
+                      labelText={'Số điện thoại'}
+                      inputType={'text'}
+                      placeholder={'Nhập số điện thoại'}
+                      inputClassName={'form-control'}
                       isRequiredField
                       isTouched={touched.phone}
                       isError={errors.phone && touched.phone}
                       errorMessage={errors.phone}
                     />
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.email}
-                      onBlur={handleBlur("email")}
-                      onChange={handleChange("email")}
-                      inputID={"email"}
-                      labelText={"Email"}
-                      inputType={"email"}
-                      placeholder={"Nhập email"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('email')}
+                      onChange={handleChange('email')}
+                      inputID={'email'}
+                      labelText={'Email'}
+                      inputType={'email'}
+                      placeholder={'Nhập email'}
+                      inputClassName={'form-control'}
                       isRequiredField
                       isTouched={touched.email}
                       isError={errors.email && touched.email}
@@ -183,76 +185,76 @@ const SettingGeneralPage = ({ t, location }) => {
                   </div>
                   <div className="row">
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.taxCode}
-                      onBlur={handleBlur("taxCode")}
-                      onChange={handleChange("taxCode")}
-                      inputID={"taxCode"}
-                      labelText={"Mã số thuế"}
-                      inputType={"text"}
-                      placeholder={"Nhập mã số thuế"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('taxCode')}
+                      onChange={handleChange('taxCode')}
+                      inputID={'taxCode'}
+                      labelText={'Mã số thuế'}
+                      inputType={'text'}
+                      placeholder={'Nhập mã số thuế'}
+                      inputClassName={'form-control'}
                     />
                     <CommonSelectInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.provinceId}
-                      onBlur={handleBlur("provinceId")}
-                      onChange={handleChange("provinceId")}
-                      inputID={"provinceId"}
-                      labelText={"Tỉnh/Thành phố"}
-                      selectClassName={"form-control"}
-                      placeholder={"Chọn Tỉnh/Thành phố"}
+                      onBlur={handleBlur('provinceId')}
+                      onChange={handleChange('provinceId')}
+                      inputID={'provinceId'}
+                      labelText={'Tỉnh/Thành phố'}
+                      selectClassName={'form-control'}
+                      placeholder={'Chọn Tỉnh/Thành phố'}
                       lstSelectOptions={getListOfProvinces()}
                     />
                   </div>
 
                   <div className="row">
                     <CommonSelectInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.districtId}
-                      onBlur={handleBlur("districtId")}
-                      onChange={handleChange("districtId")}
-                      inputID={"districtId"}
-                      labelText={"Quận huyện"}
-                      selectClassName={"form-control"}
-                      placeholder={"Chọn Quận/Huyện"}
+                      onBlur={handleBlur('districtId')}
+                      onChange={handleChange('districtId')}
+                      inputID={'districtId'}
+                      labelText={'Quận huyện'}
+                      selectClassName={'form-control'}
+                      placeholder={'Chọn Quận/Huyện'}
                       lstSelectOptions={getListOfDistricts(values.provinceId)}
                     />
                     <CommonSelectInput
-                      containerClassName={"form-group col-lg-6"}
+                      containerClassName={'form-group col-lg-6'}
                       value={values.wardId}
-                      onBlur={handleBlur("wardId")}
-                      onChange={handleChange("wardId")}
-                      inputID={"wardId"}
-                      labelText={"Phường xã"}
-                      selectClassName={"form-control"}
-                      placeholder={"Chọn Phường/Xã"}
+                      onBlur={handleBlur('wardId')}
+                      onChange={handleChange('wardId')}
+                      inputID={'wardId'}
+                      labelText={'Phường xã'}
+                      selectClassName={'form-control'}
+                      placeholder={'Chọn Phường/Xã'}
                       lstSelectOptions={getListOfWars(values.provinceId)}
                     />
                   </div>
 
                   <div className="row">
                     <CommonTextInput
-                      containerClassName={"form-group col-lg-12"}
+                      containerClassName={'form-group col-lg-12'}
                       value={values.address}
-                      onBlur={handleBlur("address")}
-                      onChange={handleChange("address")}
-                      inputID={"address"}
-                      labelText={"Địa chỉ cụ thể"}
-                      inputType={"text"}
-                      placeholder={"Nhập địa chỉ cụ thể"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('address')}
+                      onChange={handleChange('address')}
+                      inputID={'address'}
+                      labelText={'Địa chỉ cụ thể'}
+                      inputType={'text'}
+                      placeholder={'Nhập địa chỉ cụ thể'}
+                      inputClassName={'form-control'}
                     />
                   </div>
                   <div className="row">
                     <CommonMultipleTextInput
-                      containerClassName={"form-group col-lg-12"}
+                      containerClassName={'form-group col-lg-12'}
                       value={values.note}
-                      onBlur={handleBlur("note")}
-                      onChange={handleChange("note")}
-                      inputID={"note"}
-                      labelText={"Ghi chú"}
-                      inputClassName={"form-control"}
+                      onBlur={handleBlur('note')}
+                      onChange={handleChange('note')}
+                      inputID={'note'}
+                      labelText={'Ghi chú'}
+                      inputClassName={'form-control'}
                     />
                   </div>
                 </form>
--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,12 +1,14 @@
-import { combineReducers } from "redux";
-import styleReducer from "./style";
-import userReducer from "./user";
-import accountReducer from "./account";
-import headerReducer from "./header";
+import { combineReducers } from 'redux';
+import styleReducer from './style';
+import userReducer from './user';
+import accountReducer from './account';
+import headerReducer from './header';
+import locationReducer from './location';
 
 export default combineReducers({
   style: styleReducer,
   user: userReducer,
   account: accountReducer,
   header: headerReducer,
+  location: locationReducer,
 });
