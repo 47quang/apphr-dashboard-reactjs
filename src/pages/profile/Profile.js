d6cf46b420e839ecf7371ecad1f812a1d1fa1d73
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-03-19 15:00:40 +0700

    add address info

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import AddressInfo from './AddressInfo';
 import BasicInfo from './BasicInfo';
 import JobTimelineInfo from './JobTimeline';
 const Profile = ({ t, location }) => {
-  return <JobTimelineInfo />;
+  return <AddressInfo />;
 };
 
 export default Profile;
+
+{
+  /* <CContainer fluid className="c-main mb-3 px-4">
+      <div className="m-auto">
+        <div className="shadow bg-white rounded p-4">
+          <Formik>{({ values, handleBlur, handleSubmit, errors, touched }) => <form></form>}</Formik>
+        </div>
+      </div>
+    </CContainer> */
+}
