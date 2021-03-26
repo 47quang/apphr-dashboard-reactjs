2aead695c7e29607d8edce032fb57fc64babcc15
ngtrongtrung <trung.nguyen.trong2855@gmail.com>
2021-03-26 11:41:21 +0700

    add button at the bottom

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -1,6 +1,6 @@
 import { joinClassName } from './stringUtils';
 
-export const renderButtons = (buttons) => {
+export const renderButtons = (buttons, isNeedFlex = true) => {
   const leftButtons = buttons.filter((b) => b.position && b.position === 'left');
   const rightButtons = buttons.filter((b) => !leftButtons.includes(b));
   const mapButtons = (arr) =>
@@ -12,7 +12,7 @@ export const renderButtons = (buttons) => {
     ));
 
   return (
-    <div className={joinClassName(['d-flex flex-row', `justify-content-${leftButtons.length > 0 ? 'between' : 'end'}`])}>
+    <div className={isNeedFlex ? joinClassName(['d-flex flex-row', `justify-content-${leftButtons.length > 0 ? 'between' : 'end'}`]) : ''}>
       <div>{mapButtons(leftButtons)}</div>
       <div>{mapButtons(rightButtons)}</div>
     </div>
